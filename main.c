68036d0f0fb98907c29fdac8d02455e725de38d1
levin <levincozza@gmail.com>
2025-12-02 14:07:42 -0500

    now using CMake instead of Makefile

--- main.c
+++ main.c
@@ -14,8 +14,6 @@ int main(void) {
 
             ClearBackground(RAYWHITE);
 
-            DrawText("Basic window woop", 190, 200, 20, LIGHTGRAY);
-
         EndDrawing();
     }
 
@@ -23,27 +21,3 @@ int main(void) {
 
     return 0;
 }
-
-
-
-
-// #include "raylib.h"
-
-// int main(void) {
-//     InitWindow(800, 450, "Hello Raylib!");
-//     while (!WindowShouldClose()) {
-//         BeginDrawing();
-//         ClearBackground(RAYWHITE);
-//         DrawText("Hello from raylib!", 200, 200, 20, BLACK);
-//         EndDrawing();
-//     }
-//     CloseWindow();
-//     return 0;
-// }
-
-// gcc main.c -o main.exe -I"C:\raylib\include" -L"C:\raylib\lib" -lraylib -lopengl32 -lgdi32 -lwinmm
-
-// in new file build.bat (i want to use makefile instead, as it is the standard right??)
-// gcc main.c -o main.exe -I"C:\raylib\include" -L"C:\raylib\lib" -lraylib -lopengl32 -lgdi32 -lwinmm
-// .\main.exe
-// pause
